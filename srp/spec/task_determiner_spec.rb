22b7322ea34f9e595f7d3dae7033e29fc34ca4bc
deborrrrrah <dajtpbl@gmail.com>
2021-02-16 23:54:31 +0700

    [Debo] Refactor test case TaskDeterminer#task

--- srp/spec/task_determiner_spec.rb
+++ srp/spec/task_determiner_spec.rb
@@ -1,16 +1,21 @@
 require './task_determiner'
+require './employee'
 
 describe TaskDeterminer do 
+	before(:all) do
+		@employee = Employee.new("Dian", "Sasa", 3, 0.2)
+	end
+
 	it "can determine monday task" do
  		task_determiner = TaskDeterminer.new()
- 		expect(task_determiner.task("monday", "Dian Sasa")).to eq("Dian Sasa implement birthday calculator")
+ 		expect(task_determiner.task("monday", @employee)).to eq("Dian Sasa implement birthday calculator")
  	end
 	it "can determine wednesday task" do
  		task_determiner = TaskDeterminer.new()
- 		expect(task_determiner.task("wednesday", "Dian Sasa")).to eq("Dian Sasa add birthday entries")
+ 		expect(task_determiner.task("wednesday", @employee)).to eq("Dian Sasa add birthday entries")
  	end
 	it "can have default task task" do
  		task_determiner = TaskDeterminer.new()
- 		expect(task_determiner.task("friday", "Dian Sasa")).to eq("Dian Sasa will fix issues")
+ 		expect(task_determiner.task("friday", @employee)).to eq("Dian Sasa will fix issues")
  	end
 end
--- srp/task_determiner.rb
+++ srp/task_determiner.rb
@@ -1,12 +1,12 @@
 class TaskDeterminer
-	def task(date, employee_name)
+	def task(date, employee)
 		case date
 		when "monday"
-			"#{employee_name} implement birthday calculator"
+			"#{ employee.full_name } implement birthday calculator"
 		when "wednesday"
-			"#{employee_name} add birthday entries"
+			"#{ employee.full_name } add birthday entries"
 		else 
-			"#{employee_name} will fix issues"
+			"#{ employee.full_name } will fix issues"
 		end
 	end
 end
