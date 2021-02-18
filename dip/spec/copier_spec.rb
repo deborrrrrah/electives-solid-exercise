8212065423c957720643b586a3998ac8d7e40bb0
deborrrrrah <dajtpbl@gmail.com>
2021-02-18 20:32:13 +0700

    Refactor Copier#copy to reader and writer

--- dip/spec/copier_spec.rb
+++ dip/spec/copier_spec.rb
@@ -1,8 +1,15 @@
 require './copier'
+require './keyboard_reader'
+require './network_printer'
+require './printer'
+require './scanner'
 
 describe Copier do 
- it "can copy input" do
+ it "can copy input from scanner to printer" do
   copier = Copier.new
-  expect(copier.copy("SOLID Principles")).to eq("printer prints 'SOLID Principles' from keyboard")
+  scanner = Scanner.new
+  printer = Printer.new
+
+  expect(copier.copy("SOLID Principles", scanner, printer)).to eq("printer prints 'SOLID Principles' from scanner")
  end
 end
